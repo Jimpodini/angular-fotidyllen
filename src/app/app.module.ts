444e7eba9ebbc4e128d6a9eb06a204eef971cefa
Schnacken <e.schnackenburg@gmail.com>
2020-02-11 20:51:56 +0100

    Set up firebase functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LandingComponent } from './landing/landing.component';
-import { TherapistsComponent } from './therapists/therapists.component';
-import { TherapistComponent } from './therapists/therapist/therapist.component';
-import { TreatmentsComponent } from './treatments/treatments.component';
-import { ProductsComponent } from './products/products.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { BookingComponent } from './booking/booking.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { LandingComponent } from "./landing/landing.component";
+import { TherapistsComponent } from "./therapists/therapists.component";
+import { TherapistComponent } from "./therapists/therapist/therapist.component";
+import { TreatmentsComponent } from "./treatments/treatments.component";
+import { ProductsComponent } from "./products/products.component";
+import { ContactsComponent } from "./contacts/contacts.component";
+import { BookingComponent } from "./booking/booking.component";
 
 const appRoutes: Routes = [
-	{ path: 'home', component: LandingComponent },
-	{ path: 'teraputer', component: TherapistsComponent },
-	{ path: 'behandlingar', component: TreatmentsComponent },
-	{ path: 'produkter', component: ProductsComponent },
-	{ path: 'kontakt', component: ContactsComponent },
-	{ path: 'boka', component: BookingComponent },
-	{ path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: "home", component: LandingComponent },
+  { path: "teraputer", component: TherapistsComponent },
+  { path: "behandlingar", component: TreatmentsComponent },
+  { path: "produkter", component: ProductsComponent },
+  { path: "kontakt", component: ContactsComponent },
+  { path: "boka", component: BookingComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" }
 ];
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		NavbarComponent,
-		LandingComponent,
-		TherapistsComponent,
-		TherapistComponent,
-		TreatmentsComponent,
-		ProductsComponent,
-		ContactsComponent,
-		BookingComponent
-	],
-	imports: [ BrowserModule, RouterModule.forRoot(appRoutes) ],
-	providers: [],
-	bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LandingComponent,
+    TherapistsComponent,
+    TherapistComponent,
+    TreatmentsComponent,
+    ProductsComponent,
+    ContactsComponent,
+    BookingComponent
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
