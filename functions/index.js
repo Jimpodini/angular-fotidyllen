24a44decfe96b1ae686ec4678faaa94ca2968d68
Schnacken <e.schnackenburg@gmail.com>
2020-02-13 20:52:14 +0100

    add files and set up test

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,18 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
+const router = express.Router();
 
-app.get("/api", (req, res) => {
-  res.send("Jimpo");
+require("./startup/db")();
+require("./startup/validation")();
+require("./startup/middleware")(app);
+require("./startup/routes")(app);
+
+router.get("/test", (req, res) => {
+  res.send("hejsan");
 });
+
+app.use("/api", router);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
