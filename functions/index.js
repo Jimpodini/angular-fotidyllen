aaf8170146a9ef212e76cfb99afff27357022cfa
Schnacken <e.schnackenburg@gmail.com>
2020-02-13 21:03:31 +0100

    node api up and running

--- functions/index.js
+++ functions/index.js
@@ -6,15 +6,8 @@ const router = express.Router();
 require("./startup/db")();
 require("./startup/validation")();
 require("./startup/middleware")(app);
-require("./startup/routes")(app);
-
-router.get("/test", (req, res) => {
-  res.send("hejsan");
-});
+require("./startup/routes")(router);
 
 app.use("/api", router);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.app = functions.https.onRequest(app);
--- functions/startup/routes.js
+++ functions/startup/routes.js
@@ -1,7 +1,7 @@
 const auth = require("../routes/auth");
 const user = require("../routes/user");
 
-module.exports = function registerRoutes(app) {
-  app.use("/api/auth", auth);
-  app.use("/api/user", user);
+module.exports = function registerRoutes(router) {
+  router.use("/auth", auth);
+  router.use("/user", user);
 };
